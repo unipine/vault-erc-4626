728db6d589da3f5a02437fdaca0b69eebcea357b
thomas-waite <thomas.waite4@gmail.com>
2022-04-21 23:27:53 +0200

    test: add create safe and deposit test

--- src/test/BuildYieldStrategy.t.sol
+++ src/test/BuildYieldStrategy.t.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
 import {DSTest} from "ds-test/test.sol";
@@ -7,12 +6,26 @@ import {console} from "./utils/Console.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {FuseERC4626} from "fuse-flywheel/vaults/fuse/FuseERC4626.sol";
 import {Addresses} from "./utils/Addresses.sol";
+import {TurboMaster, ERC20, TurboSafe} from "tribe-turbo/TurboMaster.sol";
+import {TurboBooster} from "tribe-turbo/modules/TurboBooster.sol";
+import "forge-std/console.sol";
 
 /// @notice Build yield generating strategies with Turbo and ERC-4626
 contract BuildYieldStrategy is DSTest {
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
 
-    function setUp() public {}
+    TurboMaster turboMaster;
+    TurboBooster turboBooster;
+
+    // TODO: Add support for a new asset
+    address constant balAddress = 0xba100000625a3754423978a60c9317c58a424e3D;
+    address constant balDAO = 0xb618F903ad1d00d6F7b92f5b0954DcdC056fC533;
+    ERC20 bal = ERC20(balAddress);
+
+    function setUp() public {
+        turboMaster = TurboMaster(Addresses.TURBO_MASTER_ADDRESS);
+        turboBooster = TurboBooster(Addresses.TURBO_BOOSTER_ADDRESS);
+    }
 
     /// @notice Create an ERC-4626 strategy
     function testDeployERC4626Strategy() public {
@@ -27,13 +40,29 @@ contract BuildYieldStrategy is DSTest {
     /// @notice Create a Turbo safe, to use the created strategy
     function testCreateTurboSafeWithStrategy() public {
         vm.prank(Addresses.TURBO_ADMIN_ADDRESS);
-
-        // Create a Safe, with supported collateral
+        (TurboSafe safe, uint256 safeId) = turboMaster.createSafe(bal);
+        assertGt(safeId, 0);
     }
 
     /// @notice Deposit collateral into the Turbo Safe
     function testDepositCollateralIntoStrategy() public {
         // Deposit into safe, verify deposited collateral
+        vm.prank(Addresses.TURBO_ADMIN_ADDRESS);
+        (TurboSafe safe, uint256 safeId) = turboMaster.createSafe(bal);
+
+        uint256 amount = 100;
+        address receiver = address(1);
+
+        console.log("created");
+        // Deposit collateral
+        vm.startPrank(balDAO);
+        bal.approve(address(safe), amount);
+        safe.deposit(amount, receiver);
+        vm.stopPrank();
+
+        // Validate receiver was minted Safe Tokens
+        uint256 receiverBalance = safe.balanceOf(receiver);
+        assertGt(receiverBalance, 0);
     }
 
     /// @notice Boost the Safe (draw a loan out against it) and earn yield against that
--- src/test/utils/Addresses.sol
+++ src/test/utils/Addresses.sol
@@ -6,6 +6,10 @@ library Addresses {
     address constant TURBO_BOOSTER_ADDRESS =
         0xf6C7f4a90b10c9EaAF2A6676CE81fe8673453e72;
     address constant TURBO_ADMIN_ADDRESS =
-        0xf6C7f4a90b10c9EaAF2A6676CE81fe8673453e72;
+        0x64c4Bffb220818F0f2ee6DAe7A2F17D92b359c5d;
     address constant C_TOKEN = 0x081E7C60bCB8A2e7E43076a2988068c0a6e69e27;
+    address constant TURBO_FUSE_POOL =
+        0x081E7C60bCB8A2e7E43076a2988068c0a6e69e27;
+    address constant TURBO_ROUTER_ADDRESS =
+        0x550b756ad4FbeF34db5ac84aD41f9Cf3c8927d33;
 }
