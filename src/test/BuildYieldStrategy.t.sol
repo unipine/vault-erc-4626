250af25a6d01aeea50c83a89ef338448b18d4785
thomas-waite <thomas.waite4@gmail.com>
2022-04-22 14:09:24 +0200

    feat: add flywheel test

--- src/test/BuildYieldStrategy.t.sol
+++ src/test/BuildYieldStrategy.t.sol
@@ -8,6 +8,12 @@ import {FuseERC4626} from "fuse-flywheel/vaults/fuse/FuseERC4626.sol";
 import {Addresses} from "./utils/Addresses.sol";
 import {TurboMaster, ERC20, TurboSafe} from "tribe-turbo/TurboMaster.sol";
 import {TurboBooster} from "tribe-turbo/modules/TurboBooster.sol";
+import {FuseFlywheelCore, IFlywheelBooster, Authority} from "fuse-flywheel/FuseFlywheelCore.sol";
+import {FuseFlywheelDynamicRewards} from "fuse-flywheel/rewards/FuseFlywheelDynamicRewards.sol";
+
+abstract contract CErc20 is ERC20 {
+    function mint(uint256 amount) external virtual returns (uint256);
+}
 
 /// @notice Build yield generating strategies with Turbo and ERC-4626
 contract BuildYieldStrategy is DSTest {
@@ -15,7 +21,7 @@ contract BuildYieldStrategy is DSTest {
 
     TurboMaster turboMaster;
     TurboBooster turboBooster;
-    FuseERC4626 degenStrat;
+    FuseERC4626 somethingDegen;
 
     address constant balAddress = 0xba100000625a3754423978a60c9317c58a424e3D;
     address constant balDAO = 0xb618F903ad1d00d6F7b92f5b0954DcdC056fC533;
@@ -33,15 +39,15 @@ contract BuildYieldStrategy is DSTest {
 
         // Deploy a yield generating strategy that Alice wants to be able
         //  to invest the Fei into
-        string memory name = "degenStrategy";
+        string memory name = "somethingDegen";
         string memory symbol = "degen";
 
         address fusePool18CToken = 0x17b1A2E012cC4C31f83B90FF11d3942857664efc;
-        degenStrat = new FuseERC4626(fusePool18CToken, name, symbol);
+        somethingDegen = new FuseERC4626(fusePool18CToken, name, symbol);
 
         // Configure the strategy. Set a boost cap of $2M
         vm.prank(Addresses.TURBO_ADMIN_ADDRESS);
-        turboBooster.setBoostCapForVault(degenStrat, 2_000_000e18); // 2M, in units of Fei
+        turboBooster.setBoostCapForVault(somethingDegen, 2_000_000e18); // 2M, in units of Fei
     }
 
     /// @notice Create a Turbo safe, to use the created strategy
@@ -74,7 +80,6 @@ contract BuildYieldStrategy is DSTest {
 
         // Validate Alice received her tokens!
         uint256 aliceBalance = safe.balanceOf(receiver);
-        console.log("Alice received ", aliceBalance);
         assertGt(aliceBalance, 0);
     }
 
@@ -103,8 +108,43 @@ contract BuildYieldStrategy is DSTest {
         //    and deposit it into the strategy to earn yield
         uint256 boostAmount = 3100e18; // units of fei, ~$4k
         vm.prank(alice);
-        safe.boost(degenStrat, boostAmount);
+        safe.boost(somethingDegen, boostAmount);
 
-        assertEq(degenStrat.balanceOf(address(safe)), boostAmount);
+        assertEq(somethingDegen.balanceOf(address(safe)), boostAmount);
+    }
+
+    //////////  FLYWHEEL /////////////
+
+    /// @notice Setup a flywheel for a fuse pool
+    function testAddAFlywheelToAFusePool() public {
+        // Get the comptroller for a fuse pool that we'll add rewards to: Pool 156
+        Comptroller comptroller = Comptroller(
+            Addresses.FUSE_POOL_156_COMPTROLLER_ADDRESS
+        );
+
+        // Get a reward token that we will distribute to the pool
+        ERC20 rewardToken = ERC20(Addresses.CONVEX_TOKEN);
+
+        // Deploy the flywheel core - handles accounting
+        FuseFlywheelCore flywheel = new FuseFlywheelCore(
+            rewardToken,
+            FuseFlywheelDynamicRewards(address(0)),
+            IFlywheelBooster(address(0)),
+            address(this),
+            Authority(address(0))
+        );
+
+        // Create the rewards module - this particular module will  transfers rewards linearly
+        // over a reward cyle (7 days here)
+        rewards = new FuseFlywheelDynamicRewards(flywheel, 7 days);
+
+        // Hook up to the flywheel
+        flywheel.setFlywheelRewards(rewards);
+
+        // Attach a fuse pool to the flywheel, to distribute rewards
+        CErc20 fusePool = CErc20(Addresses.fUST3_POOL);
+        flywheel.addMarketForRewards(fusePool);
+        vm.prank(comptroller.admin());
+        comptroller._addRewardsDistributor(address(flywheel));
     }
 }
--- src/test/utils/Addresses.sol
+++ src/test/utils/Addresses.sol
@@ -12,4 +12,8 @@ library Addresses {
         0x081E7C60bCB8A2e7E43076a2988068c0a6e69e27;
     address constant TURBO_ROUTER_ADDRESS =
         0x550b756ad4FbeF34db5ac84aD41f9Cf3c8927d33;
+    address constant CONVEX_TOKEN = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
+    address constant fUST3_POOL = 0xEee0de9187B8B1Ba554E406d0b36a807A00B0ea5;
+    address constant FUSE_POOL_156_COMPTROLLER_ADDRESS =
+        0x07cd53380FE9B2a5E64099591b498c73F0EfaA66;
 }
